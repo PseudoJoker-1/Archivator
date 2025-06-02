0a29b6f3adb35854e1b859170fa74f2c2241746b
PseudoJoker-1 <armaha2302@gmail.com>
2025-06-02 21:51:35 +0500

    Add files via upload

--- exam/compressor.cpp
+++ exam/compressor.cpp
@@ -11,17 +11,24 @@ void compress(const std::string& inputFile, const std::string& outputFile) {
     std::string content((std::istreambuf_iterator<char>(in)), {});
     string output;
     int cnt = 0;
+    int j;
     for (int i = 0; i < content.length(); i++) {
-        for (int j = 0; j < content.length(); i++) {
-            if(content[i])
-        }
-        if (i != 0) {
-            if (content[i] == content[i - 1]) {
-                cnt += 1;
-                output += content[i];
-                output += to_string(cnt);
-                cout << content[i];
+        cout << i << endl;
+        for (int j = 0; j < content.length(); j++) {
+
+            if (j != i) {
+                cout << j << endl;
+                if (content[i] == content[j]) {
+                    cnt += 1;
+                    output += content[i];
+                    output += to_string(cnt);
+                    
+                    
+                }
+                
+                cout << output << endl;
             }
+
         }
     }
 
